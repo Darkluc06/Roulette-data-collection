338577c2c60204e38e92430966a02c5a31f5f9b0
Luc <Luczuidema@edulock.nl>
2024-05-14 23:36:25 +0200

    first progress for roulette betting

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-echo "hello";
+function roulette_numbers($active_numbers){
+    foreach ($active_numbers as $number) {
+        echo $number, "<br>";
+    }
+}
+
+
+$bets = [true];
+
+function betting($bets){
+    $numbers = array();
+    for ($i = 0; $i <= 36; $i++) {
+        ${"number_" . $i} = $bets[0];
+
+        array_push($numbers, ${"number_" . $i});
+    }
+    return $numbers;
+}
+
+$numbers = betting($bets);
+
+
+
+roulette_numbers($numbers);
